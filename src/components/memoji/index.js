212aedf29439c635c980167508102c4cdb8c17f1
Dalton Licause <licause@live.com>
2022-06-14 13:52:01 -0500

    set memoji size

--- src/components/memoji/index.js
+++ src/components/memoji/index.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { MemojiContainer } from './styled'
 import MemojiWEBM from "../../assets/videos/memoji.webm";
 import MemojiMOV from "../../assets/videos/memoji.mov";
 import MemojiPNG from "../../assets/imgs/memoji.png";
 
 const Memoji = () => (
-  <video playsInline autoPlay muted loop> 
-    <source src={MemojiMOV} type="video/quicktime"/>
-    <source src={MemojiWEBM} type="video/webm"/>
-    <img src={MemojiPNG} alt="Dalton's Memoji"/>
-  </video>
+  <MemojiContainer>
+    <video playsInline autoPlay muted loop> 
+      <source src={MemojiMOV} type="video/quicktime"/>
+      <source src={MemojiWEBM} type="video/webm"/>
+      <img src={MemojiPNG} alt="Dalton's Memoji"/>
+    </video>
+  </MemojiContainer>
 )
 
 export default Memoji
---
+++ src/components/memoji/styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const MemojiContainer = styled.div`
+  width: 113px;
+  height: 113px;
+`;
