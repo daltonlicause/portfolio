62d47f3a32650eb64229d7370ba379ebddede1f2
Dalton Licause <dlicause@onecountry.com>
2025-03-01 22:54:01 -0600

    optimize navigation styles for smaller screens ≤480px

--- src/components/social/styled.ts
+++ src/components/social/styled.ts
@@ -4,15 +4,24 @@ export const SocialContainer = styled.div`
   margin: 10px 0;
   display: flex;
   justify-content: center;
-  align-items: spaced-evenly;
 
   a {
+    display: flex;
     color: ${({theme}) => theme.text};
     padding: 0 6px;
     margin: 0 2px;
     font-size: 22px;
     &:hover {
       color: ${({theme}) => theme.accent}
+    }
+  }
+
+  @media (max-width: 480px) {
+    margin: 20px 0;
+    justify-content: start;
+    a {
+      padding: 8px 8px 8px 0px;
+      margin: 0 8px 0 0;
     }
   }
 `;
